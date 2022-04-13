eee8caff4197339c77ba5aa73150ab231b9bee62
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-04-13 11:38:52 +0700

    tambah variabel

--- MenuMaster_WPF/MenuMaster_WPF/RegistrasiWindow.xaml.cs
+++ MenuMaster_WPF/MenuMaster_WPF/RegistrasiWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace MenuMaster_WPF
 {
@@ -22,6 +23,47 @@ namespace MenuMaster_WPF
         public RegistrasiWindow()
         {
             InitializeComponent();
+        }
+
+        private void txt_Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_NamaAw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_NamaAk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_Pass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_PassConfirm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        public void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            txt_Username.Text = txt_Username.Text;
+            txt_NamaAw.Text = txt_NamaAw.Text;
+            txt_NamaAk.Text = txt_NamaAk.Text;
+            txt_Email.Text = txt_Email.Text;
+            txt_Pass.Text = txt_Pass.Text;
+            txt_PassConfirm.Text = txt_PassConfirm.Text;
+           
         }
     }
 }
